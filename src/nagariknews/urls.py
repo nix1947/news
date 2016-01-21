39cefc9386a617203ec8533f23b531b47393a96c
Manoj Gautam <surfer.manoj@gmail.com>
2016-01-21 20:37:22 +0545

    nagariknews urls and views created with project skel

---
+++ src/nagariknews/urls.py
@@ -0,0 +1,34 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'politics/$', views.politics, name="politics"),
+	url(r'sport/', views.sport, name="sport"),
+	url(r'abroad/', views.abroad, name="abroad"),
+	url(r'technology/', views.technology, name="technology"),
+	url(r'art/', views.art, name="art"),
+	url(r'metropolitan/', views.metropolitan, name="metropolitan"),
+	url(r'interview/', views.interview, name="interview"),
+	url(r'foreignemployment/', views.foreignemployment, name="foreignemployment"),
+	url(r'opinion/', views.opinion, name="opinion"),
+	url(r'blog/', views.blog, name="blog"),
+	url(r'health/', views.health, name="health"),
+	url(r'education/', views.education, name="education"),
+	url(r'society/', views.society, name="society"),
+	url(r'video/', views.video, name="video"),
+	url(r'lifestyle/', views.lifestyle, name="lifestyle"),
+	url(r'friday/', views.friday, name="friday"),
+	url(r'saturday/', views.saturday, name="saturday"),
+	url(r'junkiri/', views.junkiri, name="junkiri"),
+]
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/nagariknews/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def politics(request):
+	return HttpResponse("hello politics")
+
+def sport(request):
+	return HttpResponse("hello politics")
+
+def abroad(request):
+	return HttpResponse("hello politics")
+
+def technology(request):
+	return HttpResponse("hello politics")
+
+def art(request):
+	return HttpResponse("hello politics")
+
+def metropolitan(request):
+	return HttpResponse("hello politics")
+
+def interview(request):
+	return HttpResponse("hello politics")
+
+def foreignemployment(request):
+	return HttpResponse("hello politics")
+
+def opinion(request):
+	return HttpResponse("hello politics")
+
+def blog(request):
+	return HttpResponse("hello politics")
+
+def health(request):
+	return HttpResponse("hello politics")
+
+def education(request):
+	return HttpResponse("hello politics")
+
+def society(request):
+	return HttpResponse("hello politics")
+
+def video(request):
+	return HttpResponse("hello politics")
+
+def lifestyle(request):
+	return HttpResponse("hello politics")
+
+def friday(request):
+	return HttpResponse("hello politics")
+
+def saturday(request):
+	return HttpResponse("hello politics")
+
+def junkiri(request):
+	return HttpResponse("hello politics")
