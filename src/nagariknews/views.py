c3e6263608088b1cc5c0944b2247ca146935f4b3
Manoj Gautam <surfer.manoj@gmail.com>
2016-01-21 23:13:48 +0545

    summary urls and views all created

--- src/nagariknews/views.py
+++ src/nagariknews/views.py
@@ -1,56 +1,110 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import feedparser, json
+from bs4 import BeautifulSoup
+
+def parser(url):
+    feed = feedparser.parse(url)
+    #list to push dict objects
+    news = list()
+
+    for item in feed['entries']:
+        data = dict()
+        data['title'] = item.title
+        data['link'] = item.link
+        #start index for finding image url
+        start = item.description.find('http')
+        #end index for finding the image extension
+        end = item.description.find('jpg')
+        #constructing a image url form start and end
+        data['thumbnail'] = item.description[start:end+3] or None
+        #extracting summary
+        soup = BeautifulSoup(item.description, 'html5lib')
+        data['summary'] = soup.content
+        news.append(data)
+
+    #creating a json data 
+    return json.dumps(news)
+
 
 def politics(request):
-	return HttpResponse("hello politics")
+    news = parser('http://www.nagariknews.com/politics.feed')
+    return HttpResponse(news, content_type='application/json')
 
+def economy(request):
+    news = parser('http://www.nagariknews.com/economy.feed')
+    return HttpResponse(news, content_type='application/json')
+
+  
 def sport(request):
-	return HttpResponse("hello politics")
+    news = parser('http://www.nagariknews.com/sports.feed')
+    return HttpResponse(news, content_type='application/json')
 
 def abroad(request):
-	return HttpResponse("hello politics")
+    news = parser('http://www.nagariknews.com/dispora.feed')
+    return HttpResponse(news, content_type='application/json')
 
 def technology(request):
-	return HttpResponse("hello politics")
+    news = parser('http://www.nagariknews.com/technology.feed')
+    return HttpResponse(news, content_type='application/json')
 
 def art(request):
-	return HttpResponse("hello politics")
+    news = parser('http://www.nagariknews.com/entertainment.feed')
+    return HttpResponse(news, content_type='application/json')
 
 def metropolitan(request):
-	return HttpResponse("hello politics")
+    news = parser('http://www.nagariknews.com/metropolitan.feed')
+    return HttpResponse(news, content_type='application/json')
 
 def interview(request):
-	return HttpResponse("hello politics")
+    news = parser('http://www.nagariknews.com/interview.feed')
+    return HttpResponse(news, content_type='application/json')
+    
 
 def foreignemployment(request):
-	return HttpResponse("hello politics")
+    news = parser('http://www.nagariknews.com/human-resource.feed')
+    return HttpResponse(news, content_type='application/json')
+    
 
 def opinion(request):
-	return HttpResponse("hello politics")
+    news = parser('http://www.nagariknews.com/opinion.feed')
+    return HttpResponse(news, content_type='application/json')
+    
 
 def blog(request):
-	return HttpResponse("hello politics")
+    news = parser('http://www.nagariknews.com/blog.feed')
+    return HttpResponse(news, content_type='application/json')
+    
 
 def health(request):
-	return HttpResponse("hello politics")
-
+    news = parser('http://www.nagariknews.com/health.feed')
+    return HttpResponse(news, content_type='application/json')
+    
 def education(request):
-	return HttpResponse("hello politics")
-
+    news = parser('http://www.nagariknews.com/education.feed')
+    return HttpResponse(news, content_type='application/json')
+    
 def society(request):
-	return HttpResponse("hello politics")
+    news = parser('http://www.nagariknews.com/social.feed')
+    return HttpResponse(news, content_type='application/json')   
 
-def video(request):
-	return HttpResponse("hello politics")
-
+def videos(request):
+    news = parser('http://www.nagariknews.com/video.feed')
+    return HttpResponse(news, content_type='application/json')
+    
 def lifestyle(request):
-	return HttpResponse("hello politics")
-
+    news = parser('http://www.nagariknews.com/life-style.feed')
+    return HttpResponse(news, content_type='application/json')
+    
 def friday(request):
-	return HttpResponse("hello politics")
-
+    news = parser('http://www.nagariknews.com/friday.feed')
+    return HttpResponse(news, content_type='application/json')
+    
 def saturday(request):
-	return HttpResponse("hello politics")
-
+    news = parser('http://www.nagariknews.com/nagarik-sanibar.feed')
+    return HttpResponse(news, content_type='application/json')
+    
 def junkiri(request):
-	return HttpResponse("hello politics")
+    news = parser('http://www.nagariknews.com/feeds/section/34/junkiri.feed')
+    return HttpResponse(news, content_type='application/json')
+    
--- src/nnews/urls.py
+++ src/nnews/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'api/nagariknews', include('nagariknews.urls', namespace="nagariknews")),
+	url(r'apis/nagariknews', include('nagariknews.urls', namespace="nagariknews")),
     url(r'^admin/', admin.site.urls),
 ]
